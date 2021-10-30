adee9aa4022e8ab132d966e5c4fcefd02410d7c0
oriys <oriys@users.noreply.github.com>
2021-10-30 14:24:04 +0000

    deploy: oriys/posts@35b22001cb54f548f6746f1f3e168a71b77c8038

--- rua.py
+++ rua.py
@@ -39,6 +39,7 @@ movies = list(set(
         'https://movie.douban.com/subject/10604246',
         'https://movie.douban.com/subject/1292220',
         'https://movie.douban.com/subject/1463371',
+        'https://movie.douban.com/subject/10512661',
         'https://movie.douban.com/subject/34927946',
         'https://movie.douban.com/subject/25785114',
         'https://movie.douban.com/subject/25790761',
